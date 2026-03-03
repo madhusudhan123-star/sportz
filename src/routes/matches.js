d491449c0c418561cfb1472eff9094070baeb517
madhusudhan-lang <madhusudhan@doctorite.ai>
2026-03-03 16:41:37 +0530

    implement the web socket server

--- src/routes/matches.js
+++ src/routes/matches.js
@@ -48,6 +48,9 @@ matchRouter.post('/', async (req,res) =>{
             awayScore: awayScore ?? 0,
             status: getMatchStatus(startTime, endTime)
         }).returning();
+        if(res.app.locals.broadcastMatchCreated){
+            res.app.locals.broadcastMatchCreated(event);
+        }
         res.status(201).json({data: event})
     }catch (e){
         res.status(500).json({error:"Internal Server Error", details: e.message ?? String(e)})
