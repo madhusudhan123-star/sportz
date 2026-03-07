29c20a5dca359058c707246d5f8f1e93caab4863
madhusudhan-lang <madhusudhan@doctorite.ai>
2026-03-07 18:59:35 +0530

    added

--- src/ws/server.js
+++ src/ws/server.js
@@ -20,9 +20,23 @@ export function attachWebSocketServer(server){
         maxPayloadSize: 1024 * 1024,
     })
     wss.on('connection', (socket) => {
-        sendJson(socket, {type:"welcome"})
-        socket.on('error', console.error);
+        socket.isAlive = true;
+        socket.on('pong', () => {
+            socket.isAlive = true;
+        })
+        sentJson(socket, {type: "welcome"})
+        socket.on('error', console.error)
     })
+    const interval = setInterval(() => {
+        wss.clients.forEach((ws) => {
+            if(ws.isAlive === false) return ws.terminate();
+            ws.isAlive = false;
+            ws.ping();
+        })
+    }, 30000);
+
+    wss.on('close', () => clearInterval(interval));
+
     function broadcastMatchCreated(match) {
         broadcast(wss, {type:"match_created", data: match});
     }
